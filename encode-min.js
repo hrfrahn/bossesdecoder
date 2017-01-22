41c3e843335253390e39e5c6337de0679c1c90fb
Harrison Frahn <harrisonfrahn@gmail.com>
2017-01-21 16:10:42 -0800

    hopefully fix ACAO errors

--- encode-min.js
+++ encode-min.js
@@ -1,1 +1,42 @@
-function encode(){var e=new XMLHttpRequest,t=document.getElementById("Etext").value,n={text:t};return console.log(JSON.stringify(n)),e.open("POST","https://bossesdecoder.herokuapp.com/encode"),e.setRequestHeader("Content-Type","application/json"),e.send(JSON.stringify(n)),e.onreadystatechange=function(){4===this.readyState&&(200===this.status?(document.getElementById("encodedText").innerHTML="Encoded Text: "+this.responseText.replace(/^"(.+(?="$))"$/,"$1"),document.getElementById("decodedText").innerHTML=""):alert("Error"))},!1}function decode(){var e=new XMLHttpRequest,t=document.getElementById("Dtext").value,n={text:t};return console.log(JSON.stringify(n)),e.open("POST","https://bossesdecoder.herokuapp.com/decode"),e.setRequestHeader("Content-Type","application/json"),e.send(JSON.stringify(n)),e.onreadystatechange=function(){4===this.readyState&&(200===this.status?(document.getElementById("decodedText").innerHTML="Decoded Text: "+this.responseText.replace(/^"(.+(?="$))"$/,"$1"),document.getElementById("encodedText").innerHTML=""):alert("Error"))},!1}
+function encode(){
+      var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+      xmlhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+      xmlhttp.withCredentials = false;
+      var sus = document.getElementById('Etext').value;
+      var test = {"text":sus};
+      console.log(JSON.stringify(test))
+      xmlhttp.open("POST", "https://bossesdecoder.herokuapp.com/encode");
+      xmlhttp.setRequestHeader("Content-Type", "application/json");
+      xmlhttp.send(JSON.stringify(test));
+      xmlhttp.onreadystatechange= function() {
+          if (this.readyState!==4) return; // not ready yet
+          if (this.status===200) { // HTTP 200 OK
+              document.getElementById('encodedText').innerHTML = "Encoded Text: "+this.responseText.replace(/^"(.+(?="$))"$/, '$1');
+            document.getElementById('decodedText').innerHTML = "";
+          } else {
+             alert("Error")
+          }
+      };
+      return false;
+}
+		function decode(){
+			var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+      xmlhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+      xmlhttp.withCredentials = false;
+      var sus = document.getElementById('Dtext').value;
+      var test = {"text":sus};
+      console.log(JSON.stringify(test))
+      xmlhttp.open("POST", "https://bossesdecoder.herokuapp.com/decode");
+      xmlhttp.setRequestHeader("Content-Type", "application/json");
+      xmlhttp.send(JSON.stringify(test));
+      xmlhttp.onreadystatechange= function() {
+          if (this.readyState!==4) return; // not ready yet
+          if (this.status===200) { // HTTP 200 OK
+              document.getElementById('decodedText').innerHTML = "Decoded Text: "+this.responseText.replace(/^"(.+(?="$))"$/, '$1');
+            document.getElementById('encodedText').innerHTML = "";
+          } else {
+             alert("Error")
+          }
+      };
+      return false;
+		}
